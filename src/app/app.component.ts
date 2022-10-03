47488c1a40620b60040f533f8bc56dd1135a158e
DaedalusAlpha <mattrmcc@gmail.com>
2022-10-03 14:38:16 -0500

    modified:   src/app/app.component.html 	modified:   src/app/app.component.ts 	modified:   src/app/app.module.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,50 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'lite-bright';
+  size: number = 10;
+  alphabet: string[] = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+  ];
+  chosenColor: string = '';
+
+  setColor(c: string): void {
+    this.chosenColor = c;
+  }
+
+  numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  lb_grid: boolean[][] = [];
+
+  lb_col: number[] = [];
+
+  lb_row: number[] = Array.from(Array(10).keys());
 }
